8a4db8f196c06c4f4df3bff0942b0df798eb6d37
Fernando Fiani <fernandonokou@gmail.com>
2025-03-24 09:22:29 -0300

    adiciona 'aula03' e aprendemos um pouco sobre lacos de repeticao e fizemos alguns exercicios sobre

---
+++ Aula-03/src/aula/pkg03/Exercicio5Loop.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula.pkg03;
+
+/**
+ *
+ * @author CAMARGO
+ */
+import java.util.Scanner;
+public class Exercicio5Loop {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite sua primeira nota");
+        int nota1 = entrada.nextInt();
+        System.out.println("Digite sua segunda nota");
+        int nota2 = entrada.nextInt();
+        if (nota1 >= 0 && nota1 <=10 && nota2 >=0 && nota2<=10)
+        {
+            double media = (nota1+nota2)/2;
+            System.out.println("A média das suas notas é de:"+media);
+        }   
+        else{
+            System.out.println("Alguma das notas inseridas é invalida");
+        }
+        System.out.println("'NOVO CÁLCULO (S/N)?'");
+        int novo_calculo = entrada.nextInt();
+       
+    }
+            
+}
