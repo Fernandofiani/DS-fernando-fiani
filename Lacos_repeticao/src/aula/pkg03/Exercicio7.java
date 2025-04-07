052dbf050e0b77f2a5b3fe872cb3757e194cb050
Fernando Fiani <fernandonokou@gmail.com>
2025-04-06 22:43:12 -0300

    Resolvi alguns exercicios de laços de repetição

--- Lacos_repeticao/src/aula/pkg03/Exercicio7.java
+++ Lacos_repeticao/src/aula/pkg03/Exercicio7.java
@@ -8,6 +8,15 @@ package aula.pkg03;
  *
  * @author CAMARGO
  */
-public class Exercicio6 {
+
+public class Exercicio7 {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i <= 10)
+        {
+            System.out.println(i +"");
+            i++;
+        }
+    }
     
 }
